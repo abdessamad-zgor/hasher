4a0a127e35b76586d06366026581eb56c6317497
Abdessamad Zgor <abdessamadzgor@gmail.com>
2025-08-21 00:33:53 +0100

    it worked

--- libhasher.c
+++ libhasher.c
@@ -5,6 +5,21 @@
 
 #define MD5_SIZE 16
 
+char *to_hex_repr(unsigned char *data, int length)
+{
+    int i = 0;
+    char *base = "0123456789abcdef";
+    int lenbase = strlen(base);
+    char *repr = malloc(length * 2 * sizeof(char));
+    while(i < length)
+    {
+        repr[i] = base[(int)data[i] / lenbase];
+        repr[i + 1] = base[(int)data[i] % lenbase];
+        i += 2;
+    }
+    return repr;
+}
+
 char *md5_hash(char *str) 
 {
     hash_state hs;
@@ -16,6 +31,6 @@ char *md5_hash(char *str)
     md5_process(&hs, str, len);
     md5_done(&hs, tc_out);
 
-    out = tc_out;
+    out = to_hex_repr(tc_out, MD5_SIZE);
     return out;
 }
